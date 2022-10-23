94dbdbf869fa966531768b663899194da76bbfdf
ozasban <oz6962@gmail.com>
2022-10-23 19:51:36 +0300

    first class

---
+++ Stage_0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Transactions;
+
+namespace Targil0 // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome5607();
+            Console.ReadKey();
+        }
+
+        private static void welcome5607()
+        {
+            Console.Write("Enter yoyr name:");
+            string user = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", user);
+        }
+    }
+}
