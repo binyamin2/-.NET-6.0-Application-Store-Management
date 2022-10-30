27c1cd539440ebf155155251be9d217c10094f29
binyamin_Shapira <binyaminshp1@gmail.com>
2022-10-30 19:52:52 +0200

    open 3  project

---
+++ DalFacade/DO/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO;
+
+public struct Product
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    
+    public double Price { get; set; }
+
+
+    public Catagory Catagory { get; set; }
+
+
+    public int InStock { get; set; }
+}
