5f85338d3524b19942d83dd0f45b2fd214b5269e
binyamin_Shapira <binyaminshp1@gmail.com>
2022-10-31 11:52:09 +0200

    initial the personality in the project "DalFacade" and class in project "DalList"

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -3,9 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DO;
 
 public struct OrderItem
 {
+    /// <summary>
+    /// OrderItem data
+    /// </summary>
+    public int ProdectID { get; set; }//s
+
+    public int OrderID { get; set; }
+
+    public double Price { get; set; }
+    public int Amount { get; set; }
+
+
+    /// <summary>
+    /// override string ToString
+    /// </summary>
+    public override string ToString() => $@"
+    Product ID={ProdectID}:
+    OrderID - {OrderID}
+    Price: {Price}
+    Amount in order: {Amount}
+";
+
 }
---
+++ DalList/DataSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+
+internal static class DataSource
+{
+    public readonly static Random RandomInt = new Random();
+
+    internal static DalOrderItem[]  ArrOrderItem  = new DalOrderItem[200];
+
+    internal static DalOrder[] ArrOrder = new DalOrderItem[];
+
+}
+
+
+
+
