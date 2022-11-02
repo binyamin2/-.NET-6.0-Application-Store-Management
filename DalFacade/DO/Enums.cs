1bc96a692291a6f003dffb8e4ac259f96837044f
binyamin_Shapira <binyaminshp1@gmail.com>
2022-11-02 17:23:06 +0200

    ש

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -80,5 +80,14 @@ public enum Price
     SandalsSource = 380,
     SandalsChcko = 400,
     SandalsTeva = 280,
-    SandalsCrocs = 200
+    SandalsCrocs = 200,
+
+    ElegantBlandstone = 350,
+    ElegantNike = 250,
+    ElegantGolf = 200,
+    ElegantCastro = 300,
+    BootsRedback = 450,
+    BootsBlandstone = 300,
+    BootsKangeru = 270,
+    BootsNogaEinat = 200
 }
