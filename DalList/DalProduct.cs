ee2f912ac474244b410af00c2982631d37d8a3cc
ozasban <oz6962@gmail.com>
2022-11-06 13:59:03 +0200

    mio

--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,54 @@ namespace Dal;
 
 public class DalProduct
 {
+    public int AddProduct(Product p)
+    {
+        bool flag=false;
+        for (int i = 0; i < DataSource.ArrProduct.Length; i++)
+        {
+            if (DataSource.ArrProduct[i].ID==p.ID)
+            {
+                flag=true;
+            }
+        }
+        if(!flag)
+        {
+            DataSource.AddProduct(p);
+            return p.ID;
+        }
+        else
+        {
+            return p.ID;
+        }
 
+    }
+
+    public Product getPruduct(int IDp)
+    {
+        for (int i = 0; i < DataSource.ArrProduct.Length; i++)
+        {
+            if (DataSource.ArrProduct[i].ID == IDp)
+            {
+                return DataSource.ArrProduct[i];
+            }
+        }
+        Product p = new Product();
+        p.ID = -1;
+        return p;
+    }
+
+    public void deleteProduct(int id)
+    {
+        for (int i = 0; i < DataSource.ArrProduct.Length; i++)
+        {
+            if (DataSource.ArrProduct[i].ID == id)
+            {
+                for (int j = i; j < DataSource.ArrProduct.Length - 1; j++)
+                {
+                    DataSource.ArrProduct[j] = DataSource.ArrProduct[j + 1];
+                }
+                DataSource.Config.ProductIndex--;
+            }
+        }
+    }
 }
