b9ae16ccd561e8403357f2f0afe64973a5943c32
binyamin_Shapira <binyaminshp1@gmail.com>
2022-11-20 15:41:41 +0200

    asa

--- BL/BlImplementation/Order.cs
+++ BL/BlImplementation/Order.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BlApi;
+using Dal;
+using DalApi;
+
 namespace BlImplementation;
 
 internal class Order : BlApi.IOrder
 {
+    private IDal Dal = new DalList();
 
 }
