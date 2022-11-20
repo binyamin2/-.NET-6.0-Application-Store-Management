e6da11f0b84463f6e3de24fa061338897fca5b74
ozasban <oz6962@gmail.com>
2022-11-20 15:07:21 +0200

    add interfeces

---
+++ BL/BlApi/IProduct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+namespace BlApi;
+
+public interface IProduct
+{
+    /// <summary>
+    /// get list of product
+    /// </summary>
+    /// <returns>IEnumerable<BO.Product></returns>
+    public IEnumerable<BO.Product> GetList();
+    /// <summary>
+    /// get product details for manager
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>BO.Product</returns>
+    public BO.Product GetForManager(int? id);
+    /// <summary>
+    /// get product details for client
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="cart"></param>
+    /// <returns>BO.Product</returns>
+    public BO.Product GetForClient(int? id,BO.Cart cart);
+    /// <summary>
+    /// add a product for data base   for manager
+    /// </summary>
+    /// <param name="product"></param>
+    public void Add(BO.Product product);
+    /// <summary>
+    /// delete product from data base   for manager
+    /// </summary>
+    /// <param name="id"></param>
+    public void Delete(int? id);
+    /// <summary>
+    /// update a product   for manager
+    /// </summary>
+    /// <param name="product"></param>
+    public void Update(BO.Product product);
+
+
+}
