fec23ae119c788a39a2f0302ebc082298d87aefb
binyamin_Shapira <binyaminshp1@gmail.com>
2022-11-20 12:21:50 +0200

    ADD BO.cart BO.Order Bo.ProudctItem

---
+++ BL/BO/Cart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+
+public class Cart
+{
+    public string? CustomerName { get; set; }
+    public string? CustomerEmail { get; set; }
+    public string? CustomerAdress { get; set; }
+    public List<OrderItem?> Items { get; set; }
+
+    public double? TotalPrice { get; set; }
+
+
+    public override string ToString() => $@"
+        Name :{CustomerName} 
+        Adress : {CustomerAdress}
+        Mail : {CustomerEmail}
+        TotalPrice : {TotalPrice}
+        
+";
+}
