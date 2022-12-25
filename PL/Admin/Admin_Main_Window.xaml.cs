07cfe878fd7ac42527cbd455ed7d9047065f6341
binyamin_Shapira <binyaminshp1@gmail.com>
2022-12-25 14:58:10 +0200

    add Orders list

---
+++ PL/Admin/Admin_Main_Window.xaml.cs
@@ -0,0 +1,35 @@
+﻿using BlImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Admin
+{
+    /// <summary>
+    /// Interaction logic for Admin_Main_Window.xaml
+    /// </summary>
+    public partial class Admin_Main_Window : Window
+    {
+        BlApi.IBl bl;
+        public Admin_Main_Window(BlApi.IBl bl)
+        {
+            this.bl = bl;
+            InitializeComponent();
+        }
+
+        private void ShowProductsButton_Click(object sender, RoutedEventArgs e) => new PL.Products.ProductForList(new ViewModel.ProductForListVM(bl)).Show();
+
+        private void Show_Order_Button_Click(object sender, RoutedEventArgs e) => new PL.Orders.Orders_for_list_window(new ViewModel.Orders_VM(bl)).Show();
+       
+    }
+}
---
+++ PL/ViewModel/Orders_VM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using BO;
+
+namespace PL.ViewModel;
+
+public class Orders_VM
+{
+    BlApi.IBl bl;
+
+    public Orders_VM(BlApi.IBl bl)
+    {
+        this.bl = bl;
+        orders = new ObservableCollection<BO.OrderForList> (bl.Order.GetList());
+    }
+
+    public ObservableCollection<BO.OrderForList> orders;
+
+    public ObservableCollection<BO.OrderForList> Orders
+    {
+        get { return orders; }
+        set { Set(ref orders, value); }
+    }
+
+    public void ShowUpdate(int id)
+    {
+        MessageBox.Show("hi");
+    }
+
+    public ICommand Update_Item => new RelayCommand<int>(ShowUpdate);
+
+
+
+
+    #region PropertyChanged
+    private void Set<T>(ref T prop, T val, [CallerMemberName] string? name = "")
+    {
+        if (!prop.Equals(val))
+        {
+            prop = val;
+            PropertyChanged?.Invoke(this, new(name));
+        }
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    #endregion
+
+
+}
