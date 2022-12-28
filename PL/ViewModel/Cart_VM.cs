de51fab03e032fa39ebf8ebdf07d7aba55ffa03d
ozasban <oz6962@gmail.com>
2022-12-28 15:56:24 +0200

    cart

---
+++ PL/ViewModel/Cart_VM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.ViewModel;
+
+public class Cart_VM: INotifyPropertyChanged
+{
+
+
+    #region PropertyChanged
+    private void Set<T>(ref T prop, T val, [CallerMemberName] string? name = "")
+    {
+        if (!prop.Equals(val))
+        {
+            prop = val;
+            PropertyChanged?.Invoke(this, new(name));
+        }
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    #endregion
+}
