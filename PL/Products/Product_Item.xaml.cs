a184d5e6bd3505620e32b06b5c50121658bd2c72
ozasban <oz6962@gmail.com>
2022-12-28 16:37:30 +0200

    product item

---
+++ PL/Products/Product_Item.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL.Products
+{
+    /// <summary>
+    /// Interaction logic for Product_Item.xaml
+    /// </summary>
+    public partial class Product_Item : Window
+    {
+        private Cart_VM vm;
+        public Product_Item(Cart_VM vm)
+        {
+            this.vm = vm;
+            InitializeComponent();
+        }
+    }
+}
--- PL/ViewModel/Cart_VM.cs
+++ PL/ViewModel/Cart_VM.cs
@@ -10,8 +10,48 @@ namespace PL.ViewModel;
 
 public class Cart_VM: INotifyPropertyChanged
 {
+    #region OrderWindow
+    private int productId;
+    public int ProductId
+    {
+        get { return productId; }
+        set { Set(ref productId, value); }
+    }
 
+    private string? productName = "";
+    public string? ProductName
+    {
+        get { return productName; }
+        set { Set(ref productName, value); }
+    }
 
+    private double price;
+    public double Price
+    {
+        get { return price; }
+        set { Set(ref price, value); }
+    }
+    private BO.Category? category;
+    public BO.Category? Category
+    {
+        get { return category; }
+        set { Set(ref category, value); }
+    }
+
+    private bool inStoct = false;
+    public bool InStoct
+    {
+        get { return inStoct; }
+        set { Set(ref inStoct, value); }
+    }
+
+     private string? inStoctText = "No";
+    public string? InStoctText
+    {
+        get { return inStoctText; }
+        set { Set(ref inStoctText, value); }
+    }
+    #endregion
     #region PropertyChanged
     private void Set<T>(ref T prop, T val, [CallerMemberName] string? name = "")
     {
