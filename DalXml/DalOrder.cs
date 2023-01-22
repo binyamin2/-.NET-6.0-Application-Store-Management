11a19561d86fbf19fe27b8e9467805669d727c44
ozasban <oz6962@gmail.com>
2023-01-22 13:30:29 +0200

    commit

--- DalXml/DalOrder.cs
+++ DalXml/DalOrder.cs
@@ -6,7 +6,12 @@ namespace Dal;
 internal class DalOrder : IOrder
 {
     public string s_Order = "Orders";
-    
+    /// <summary>
+    /// create order from xelement
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    /// <exception cref="FormatException"></exception>
     static DO.Order? createOrderfromXElement(XElement s)
     {
         return new DO.Order()
@@ -20,20 +25,26 @@ internal class DalOrder : IOrder
             DeliveryDate = s.ToDateTimeNullable("DeliveryDate"),
         };
     }
+    /// <summary>
+    /// adding order to orders
+    /// </summary>
+    /// <param name="order"></param>
+    /// <returns></returns>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public int Add(DO.Order order)
     {
+        //loading the root
         XElement OrderRootElem = XMLTools.LoadListFromXMLElement(s_Order);
 
         ///take "config.xml" for the index
 
         XElement ConfigRootElem = XMLTools.LoadListFromXMLElement("Config");
 
-        int id = (int)ConfigRootElem.Element("OrderIndex");
+        int id = (int)ConfigRootElem.Element("OrderIndex")!;
 
         order.ID = id;
 
-
+        //build new xelement
         XElement OrderElem = new XElement("Order",
                                    new XElement("ID", order.ID),
                                    new XElement("CustomerName", order.CustomerName),
@@ -46,65 +57,81 @@ internal class DalOrder : IOrder
 
         id++;
 
-
+        //update the id 
         ConfigRootElem.Element("OrderIndex")?.SetValue(id);
 
-
+        //save the id in the config file
         XMLTools.SaveListToXMLElement(ConfigRootElem, "Config");
 
 
 
-
+        //add and save the list
         OrderRootElem.Add(OrderElem);
 
         XMLTools.SaveListToXMLElement(OrderRootElem, s_Order);
 
         return order.ID; 
     }
-
+    /// <summary>
+    /// delete order from the file
+    /// </summary>
+    /// <param name="id"></param>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public void Delete(int id)
     {
         XElement OrderRootElem = XMLTools.LoadListFromXMLElement(s_Order);
-
+        //find the order to delete
         XElement? order = (from or in OrderRootElem.Elements()
                            where (int?)or.Element("ID") == id
                            select or).FirstOrDefault() ?? throw new NotFoundException("the Order not found");
 
-        order.Remove(); //<==>   Remove order from OrderRootElem
-
+        order.Remove(); //  Remove order from OrderRootElem
+        //save
         XMLTools.SaveListToXMLElement(OrderRootElem, s_Order);
     }
-
+    /// <summary>
+    /// get the order by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public DO.Order? Get(int id)
     {
         XElement OrderRootElem = XMLTools.LoadListFromXMLElement(s_Order);
-
+        //using linq to find and return the order
         return (from s in OrderRootElem?.Elements()
                 where s.ToIntNullable("ID") == id
                 select (DO.Order?)createOrderfromXElement(s)).FirstOrDefault()
                 ?? throw new NotFoundException("the Order not found");
     }
-
+    /// <summary>
+    /// get order by predict
+    /// </summary>
+    /// <param name="predict"></param>
+    /// <returns></returns>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public DO.Order? Get(Func<DO.Order?, bool>? predict)
     {
         XElement OrderRootElem = XMLTools.LoadListFromXMLElement(s_Order);
-
+        //using linq to find and return the order
         return (from s in OrderRootElem?.Elements()
                 where predict(createOrderfromXElement(s))
                 select (DO.Order?)createOrderfromXElement(s)).FirstOrDefault()
                 ?? throw new NotFoundException("the Order not found\"");
     }
-
+    /// <summary>
+    /// get the all file of order by predict
+    /// </summary>
+    /// <param name="predict"></param>
+    /// <returns></returns>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public IEnumerable<DO.Order?> GetAll(Func<DO.Order?, bool>? predict = null)
     {
         XElement OrderRootElem = XMLTools.LoadListFromXMLElement(s_Order);
-
-        
-
+        //return all the file
         if (predict != null)
         {
             return from s in OrderRootElem.Elements()
@@ -112,6 +139,7 @@ internal class DalOrder : IOrder
                    where predict(doOrd)
                    select (DO.Order?)doOrd;
         }
+        //return the wanted element
         else
         {
 
@@ -119,12 +147,16 @@ internal class DalOrder : IOrder
                    select createOrderfromXElement(s);
         }
     }
-
+    /// <summary>
+    /// update order
+    /// </summary>
+    /// <param name="order"></param>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public void Update(DO.Order order)
     {
         XElement OrderRootElem = XMLTools.LoadListFromXMLElement(s_Order);
-
+        //find the order to update
         XElement? new_order = (from or in OrderRootElem.Elements()
                            where (int?)or.Element("ID") == order.ID
                            select or).FirstOrDefault() ?? throw new NotFoundException("the Order not found");
@@ -136,6 +168,7 @@ internal class DalOrder : IOrder
                                    new XElement("OrderDate", order.OrderDate),
                                    new XElement("ShipDate", order.ShipDate),
                                    new XElement("DeliveryDate", order.DeliveryDate));
+        //remove the old order and add the updated order
         new_order.Remove();
 
         OrderRootElem.Add(OrderElem);
--- DalXml/DalOrderItem.cs
+++ DalXml/DalOrderItem.cs
@@ -6,6 +6,12 @@ namespace Dal;
 internal class DalOrderItem : IOrderItem
 {
     const string s_Oi = "OrderItems";
+    /// <summary>
+    /// create order item from xwlement
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns>OrderItem</returns>
+    /// <exception cref="FormatException"></exception>
     static DO.OrderItem? createOIfromXElement(XElement s)
     {
         return new DO.OrderItem()
@@ -13,21 +19,26 @@ internal class DalOrderItem : IOrderItem
             ID = s.ToIntNullable("ID") ?? throw new FormatException("id"),
             ProductID = s.ToIntNullable("ProductID") ?? throw new FormatException("product id"),
             OrderID = s.ToIntNullable("OrderID") ?? throw new FormatException("order id"),
-            Price = (double)s.Element("Price"),
+            Price = (double)s.Element("Price")!,
             Amount = s.ToIntNullable("Amount") ?? throw new FormatException("Amount")
         };
     }
+    /// <summary>
+    /// add Order Item to the file
+    /// </summary>
+    /// <param name="OI"></param>
+    /// <returns>int </returns>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public int Add(DO.OrderItem OI)
     {
         XElement OIRootElem = XMLTools.LoadListFromXMLElement(s_Oi);
 
         XElement ConfigRootElem = XMLTools.LoadListFromXMLElement("Config");
-
-        int id = (int)ConfigRootElem.Element("OrderItemIndex");
-
+        //find the current order item id and put it in OI
+        int id = (int)ConfigRootElem.Element("OrderItemIndex")!;
+        
         OI.ID = id;
-
+        //build xelement oi
         XElement OIElem = new XElement("OrderItem",
                                    new XElement("ID", OI.ID),
                                    new XElement("ProductID", OI.ProductID),
@@ -35,6 +46,7 @@ internal class DalOrderItem : IOrderItem
                                    new XElement("Price", OI.Price),
                                    new XElement("Amount", OI.Amount)
                                    );
+        //update the files
         id++;
 
         ConfigRootElem.Element("OrderItemIndex")?.SetValue(id);
@@ -47,47 +59,66 @@ internal class DalOrderItem : IOrderItem
 
         return OI.ID;
     }
-
+    /// <summary>
+    /// delete order item from file
+    /// </summary>
+    /// <param name="id"></param>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public void Delete(int id)
     {
         XElement OIRootElem = XMLTools.LoadListFromXMLElement(s_Oi);
-
+        //find th oi to delete with linq
         XElement? oi = (from st in OIRootElem.Elements()
                         where (int?)st.Element("ID") == id
                         select st).FirstOrDefault() ?? throw new NotFoundException("the orderitem not found");
 
-        oi.Remove(); //<==>   Remove Product from Productlist
+        oi.Remove(); //   Remove Product from Productlist
 
         XMLTools.SaveListToXMLElement(OIRootElem, s_Oi);
     }
-
+    /// <summary>
+    /// get order item by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>OrderItem</returns>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public DO.OrderItem? Get(int id)
     {
         XElement OIRootElem = XMLTools.LoadListFromXMLElement(s_Oi);
-
+        //get the order item by linq
         return (from s in OIRootElem?.Elements()
                 where s.ToIntNullable("ID") == id
                 select (DO.OrderItem?)createOIfromXElement(s)).FirstOrDefault()
                 ?? throw new NotFoundException("the orderitem not found");
     }
-
+    /// <summary>
+    /// get order item by predict
+    /// </summary>
+    /// <param name="predict"></param>
+    /// <returns>OrderItem</returns>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public DO.OrderItem? Get(Func<DO.OrderItem?, bool>? predict)
     {
         XElement OIRootElem = XMLTools.LoadListFromXMLElement(s_Oi);
-
+        //get the order item by linq
         return (from s in OIRootElem?.Elements()
                 where predict(createOIfromXElement(s))
                 select (DO.OrderItem?)createOIfromXElement(s)).FirstOrDefault()
                 ?? throw new NotFoundException("the orderitem not found");
     }
-
+    /// <summary>
+    /// get the all file to list by filter
+    /// </summary>
+    /// <param name="filter"></param>
+    /// <returns>IEnumerable<DO.OrderItem?></returns>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public IEnumerable<DO.OrderItem?> GetAll(Func<DO.OrderItem?, bool>? filter = null)
     {
         XElement OIRootElem = XMLTools.LoadListFromXMLElement(s_Oi);
+        //return the wanted elements
         if (filter != null)
         {
             return from s in OIRootElem.Elements()
@@ -95,6 +126,7 @@ internal class DalOrderItem : IOrderItem
                    where filter(oi)
                    select (DO.OrderItem?)oi;
         }
+        //return all file
         else
         {
             return from s in OIRootElem.Elements()
@@ -102,37 +134,53 @@ internal class DalOrderItem : IOrderItem
         }
 
     }
-
+    /// <summary>
+    /// return order item by order and product id
+    /// </summary>
+    /// <param name="orderId"></param>
+    /// <param name="productId"></param>
+    /// <returns>orderitem</returns>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public DO.OrderItem? GetItemByOrderAndProduct(int orderId, int productId)
     {
         XElement OIRootElem = XMLTools.LoadListFromXMLElement(s_Oi);
-
+        //return the wanted order item with linq
         return (from s in OIRootElem?.Elements()
                 where s.ToIntNullable("ProductID") == productId && s.ToIntNullable("OrderID") == orderId
                 select (DO.OrderItem?)createOIfromXElement(s)).FirstOrDefault()
                 ?? throw new NotFoundException("the orderitem not found");
     }
 
-
+    /// <summary>
+    /// return order item by order id
+    /// </summary>
+    /// <param name="orderId"></param>
+    /// <returns>IEnumerable<DO.OrderItem?></returns>
+    /// <exception cref="NotFoundException"></exception>
     public IEnumerable<DO.OrderItem?> GetItemsListByOrderId(int orderId)
     {
         XElement OIRootElem = XMLTools.LoadListFromXMLElement(s_Oi);
-
+        //return the wanted order item with linq
         return (from s in OIRootElem?.Elements()
                 where s.ToIntNullable("OrderID") == orderId
                 select (DO.OrderItem?)createOIfromXElement(s))
                 ?? throw new NotFoundException("the orderitem not found");
     }
-
+    /// <summary>
+    /// update order item
+    /// </summary>
+    /// <param name="oi"></param>
+    /// <exception cref="NotFoundException"></exception>
     [MethodImpl(MethodImplOptions.Synchronized)]
     public void Update(DO.OrderItem oi)
     {
         XElement OIRootElem = XMLTools.LoadListFromXMLElement(s_Oi);
-
+        //find the wanted order item
         XElement? noi = (from st in OIRootElem.Elements()
                          where (int?)st.Element("ID") == oi.ID
                          select st).FirstOrDefault() ?? throw new NotFoundException("the orderitem not found");
+        //update the details and save the file
         noi.Element("ID")!.SetValue(oi.ID);
         noi.Element("ProductID")!.SetValue(oi.ProductID);
         noi.Element("OrderID")!.SetValue(oi.OrderID);
